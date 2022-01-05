fb7807a53114676f150b7bdd36e85b268d666a91
Michael-Loren <michael.loren@cix.csi.cuny.edu>
2022-01-05 15:01:19 -0500

    add more funcs

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function myMap(arr, callback) {
 
 }
 
-const map1 = myMap(arr, x => x * 2);
+//const map1 = myMap(arr, x => x * 2);
 //console.log(map1); // output: [ 2, 8, 18, 32 ]
 
 
@@ -30,7 +30,7 @@ const map1 = myMap(arr, x => x * 2);
 function myFilter(arr, callback) {
     let newarr = [];
     for (let i = 0; i < arr.length; i++) {
-        if (callback(arr[i])){
+        if (callback(arr[i])) {
             newarr.push(arr[i]);
         }
     }
@@ -40,4 +40,43 @@ function myFilter(arr, callback) {
 const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
 const result = myFilter(words, word => word.length > 6);
-console.log(result);
+//console.log(result);
+
+
+
+
+function mySome(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (callback(arr[i])) {
+            return true;
+        }
+    }
+    return false;
+}
+
+//console.log(mySome(arr, (item) => item % 2 === 0)); //check if has even
+
+function myEvery(arr, callback) {
+    for (let i = 0; i < arr.length; i++) {
+        if (!callback(arr[i])) {
+            return false;
+        }
+    }
+    return true;
+}
+
+//console.log(myEvery(arr, (x) => x < 20)); //check if every below 20
+
+
+
+function myReduce(arr, callback){
+    let r = arr[0];
+    for (let i = 1; i < arr.length; i++){
+         r = callback(arr[i], r);
+    }
+    return r;
+}
+
+
+//console.log(arr1.reduce((x,y) => x + y));
+//console.log(myReduce(arr, (x,y) => x * y));
